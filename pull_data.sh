0cd71f07d93c5760bb1db3b6bdd31b288b31cd1e
XMR_Anon <26285802+xmranon@users.noreply.github.com>
2018-11-30 15:52:01 -0500

    perf improvement

--- pull_data.sh
+++ pull_data.sh
@@ -6,8 +6,8 @@ for i in `seq 1 $total`;
 
 do
 item=$i
-a=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":}}' -H 'Content-Type: application/json' | jq '.result.block_header.difficulty')
-b=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.timestamp' | gawk '{print strftime("%c", $0)}')
+a=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":}}' -H 'Content-Type: application/json' | jq '.result.block_header.difficulty') &
+b=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.timestamp' | gawk '{print strftime("%c", $0)}') &
 echo $i','$a','$b \ >> ~/Desktop/difficulty.csv
 percent=$(awk "BEGIN { pc=100*${item}/${total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
 clear
