f1b7b5869f57d378df51ae69c898626d7fe52529
XMR_Anon <26285802+xmranon@users.noreply.github.com>
2019-01-27 13:00:53 -0500

    added data and check for file
    This commit checks for the BlockchainDataExtract file and if it does not exist - it creates it and adds a header. If it does exist it will count the number of lines/blocks and picks up where it left off.

--- pull_data.sh
+++ pull_data.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-read -p "You ready to pull some motherFUCKING DATA? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-exit 1
+total=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_info","params":{"height":1}}' -H 'Content-Type: application/json' | jq '.result.height')
+
+#!/bin/bash
+file=~/Desktop/BlockchainDataExtract.csv
+if [ ! -e "$file" ]; then
+echo "Height" ',' "Difficulty" ',' "Timestamp" ',' "Nonce" ',' "transactions"  \ >> ~/Desktop/BlockchainDataExtract.csv
+chmod 777 ~/Desktop/BlockchainDataExtract.csv
+lines=$(1)
+else
+lines=$(wc -l < ~/Desktop/BlockchainDataExtract.csv)
 fi
 
-total=$(curl -s -X POST http://127.0.0.1:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_info","params":{"height":1}}' -H 'Content-Type: application/json' | jq '.result.height')
-
-for i in `seq 1 $total`;
+for i in `seq $lines $total`;
 
 do
 item=$i
-a=$(curl -s -X POST http://127.0.0.1:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.difficulty') &
-b=$(curl -s -X POST http://127.0.0.1:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.timestamp' | gawk '{print strftime("%c", $0)}') &
-c=$(curl -s -X POST http://127.0.0.1:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.nonce') &
-d=$(curl -s -X POST http://127.0.0.1:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.num_txs') &
-e=$(curl -s -X POST http://127.0.0.1:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.reward') &
-f=$(curl -s -X POST http://127.0.0.1:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.major_version') &
-g=$(curl -s -X POST http://127.0.0.1:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.block_size') &
-echo $i','$a','$b','$c','$d','$e','$f','$g \ >> ~/Desktop/difficulty.csv
+a=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.difficulty')
+b=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.timestamp' | gawk '{print strftime("%c", $0)}')
+c=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.nonce')
+d=$(curl -s -X POST http://127.0.0.1:17566/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'$i'}}' -H 'Content-Type: application/json' | jq '.result.block_header.num_txes')
+echo $i','$a','$b','$c','$d \ >> ~/Desktop/BlockchainDataExtract.csv
+
 percent=$(awk "BEGIN { pc=100*${item}/${total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
 clear
-echo $percent
+echo $i
 
 done
 
